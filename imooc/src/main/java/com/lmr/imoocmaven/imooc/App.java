4549aee147fb588e5bcc3252cb7922e4b5ab9658
Lemons <401103554@qq.com>
2017-06-26 08:59:21 +0800

    Imooc+Test

---
+++ imooc/src/main/java/com/lmr/imoocmaven/imooc/App.java
@@ -0,0 +1,13 @@
+package com.lmr.imoocmaven.imooc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
